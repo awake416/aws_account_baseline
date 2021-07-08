aa189d6c3030e99fbe66b0cd12045499b2fcfa63
awake416 <chhikara.vivek@gmail.com>
2021-07-09 00:29:02 +0530

    issue-7 - added client VPN endpoint config

--- src/client_vpn_endpoint/client-vpn-endpoint.tf
+++ src/client_vpn_endpoint/client-vpn-endpoint.tf
@@ -11,7 +11,7 @@ locals {
 module tls-certs {
   count = local.create_certs
   source  = "app.terraform.io/awake416/tls-certs/aws"
-  version = "0.0.6"
+  version = "0.0.7"
   subject = {
     common_name = "home-vpn"
     organization = "home"
