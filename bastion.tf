5d18d9628f1acef724405c2a49b72557b963c6d6
awake416 <chhikara.vivek@gmail.com>
2021-07-09 14:50:39 +0530

    issue-7 - added bastion

--- bastion.tf
+++ bastion.tf
@@ -2,7 +2,8 @@ variable "generate_ssh_key" {
   default = true
 }
 variable "user_data" {
-  default = "yum install -y postgresql-client-common"
+  default = [
+    "yum install -y postgresql-client-common"]
 }
 
 variable "associate_public_ip_address" {
@@ -31,29 +32,29 @@ module aws_key_pair {
 
 resource aws_secretsmanager_secret private_key {
   description = "bastion private key"
-  name = "bastion_private_key"
-  tags = {
-    Name = "bastion_private_key"
+  name        = "bastion_private_key"
+  tags        = {
+    Name        = "bastion_private_key"
     Environment = var.env
   }
 }
 
 resource aws_secretsmanager_secret_version private_key {
-  secret_id = aws_secretsmanager_secret.private_key.id
+  secret_id     = aws_secretsmanager_secret.private_key.id
   secret_string = module.aws_key_pair.private_key
 }
 
 resource aws_secretsmanager_secret public_key {
   description = "bastion public key"
-  name = "bastion_public_key"
-  tags = {
-    Name = "bastion_public_key"
+  name        = "bastion_public_key"
+  tags        = {
+    Name        = "bastion_public_key"
     Environment = var.env
   }
 }
 
 resource aws_secretsmanager_secret_version public_key {
-  secret_id = aws_secretsmanager_secret.public_key.id
+  secret_id     = aws_secretsmanager_secret.public_key.id
   secret_string = module.aws_key_pair.public_key
 }
 
