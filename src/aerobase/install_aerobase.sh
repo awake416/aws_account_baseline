2d1f218c4061cea523b1b98bb370a2a3f31f142f
awake416 <chhikara.vivek@gmail.com>
2021-07-11 15:47:44 +0530

    issue-7 - updated instance type and docker

--- src/aerobase/install_aerobase.sh
+++ src/aerobase/install_aerobase.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 sudo yum update -y
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-sudo systemctl start httpd
-sudo systemctl enable httpd
 
+sudo yum install -y docker
+sudo systemctl enable --now docker
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo yum install -y java-1.8.0-openjdk
-sudo yum install -y aerobase-2.4.2.el7.x86_64.rpm aerobase-iam-2.4.2.el7.x86_64.rpm
+
--- src/aerobase/standalone-aerobase.tf
+++ src/aerobase/standalone-aerobase.tf
@@ -46,6 +46,7 @@ module "aerobase_instance" {
   subnet                      = var.subnets[0]
   ami_owner                   = var.ami_owner
   ami                         = var.ami_id
+  instance_type               = "t4g.xlarge"
   associate_public_ip_address = false
   name                        = "aerobase-standalone"
   namespace                   = "lab"
